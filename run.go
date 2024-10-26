c2bb9e3c6e8061f835736c96e2bb6fe8048e9a18
zzp198 <zzp198@163.com>
2024-10-26 18:51:25 +0800

    services + about

--- run.go
+++ run.go
@@ -21,6 +21,15 @@ type User struct {
 	Token    string `gorm:"type:varchar(64)"`
 }
 
+var testimonial = map[string]string{
+	"Bill Danson":   "Jackie has been absolutely great. Very accommodating and a pleasure to deal with. Will be back here the next time for sure. I want to have another car wrapping for my new Ford. Great job guys!",
+	"Blank Richard": "Thanks Jackie for the awesome LED lightbox. Your great communication and attention to service are truely hard to come by these days. My partner really happy with that. Thanks sooo much I will definately be comming back for other signages and car graphics.",
+	"Shannon Wood":  "Exceptional service. Provided great help and assistance in getting sizes correct for three of my vehicles. Also ensured a date deadline was met and provided excellent communication. Outstanding designer in every respect. Thank you.",
+	"Carl Madden":   "I was very pleased with the level of professional service I received from signmate limited, the quality of the graphics for my logo was creative and perfect for my company in attracting new customers. ",
+	"Tomma":         "Excellent Service! Definitely Highly Recommended... They did amzing vehicle graphic on my Holden. These guys are really frienly and professional. I had a good service, thank you Signmate!",
+	"Mr Dan":        " Signmate Limited helped me created a ideal image for my company store, through this change I have attracted new customers and in turn created more profit.",
+}
+
 func main() {
 	r = gin.Default()
 
@@ -28,7 +37,25 @@ func main() {
 	Database()
 
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(200, "index.html", gin.H{})
+		c.Redirect(200, "/index.html")
+	})
+
+	r.GET("/index.html", func(c *gin.Context) {
+		c.HTML(200, "index.html", gin.H{
+			"testimonial": testimonial,
+		})
+	})
+
+	r.GET("/about.html", func(c *gin.Context) {
+		c.HTML(200, "about.html", gin.H{
+			"testimonial": testimonial,
+		})
+	})
+
+	r.GET("/services.html", func(c *gin.Context) {
+		c.HTML(200, "services.html", gin.H{
+			"testimonial": testimonial,
+		})
 	})
 
 	r.GET("/project", func(c *gin.Context) {
@@ -73,12 +100,7 @@ func main() {
 		c.JSON(200, gin.H{"paths": paths})
 	})
 
-	r.GET("/:any", func(c *gin.Context) {
-		if strings.HasSuffix(c.Param("any"), ".html") {
-			c.HTML(200, c.Param("any"), gin.H{})
-			return
-		}
-
+	r.NoRoute(func(c *gin.Context) {
 		c.HTML(404, "404.html", gin.H{})
 	})
 
